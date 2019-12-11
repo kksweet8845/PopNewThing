66a2b55035a6c360026f953b39bd7ec218eb2b39
f74064054@mail.ncku.edu.tw <f74064054@mail.ncku.edu.tw>
2019-12-11 15:44:42 +0800

    Delete helpInfo fn

--- utils/@paper-fsm/schema.js
+++ utils/@paper-fsm/schema.js
@@ -137,7 +137,6 @@ const schema = {
                     ]
                 }
             },
-            entry: ['helpInfo'],
             meta : {
                 listFunc : [
                     {
